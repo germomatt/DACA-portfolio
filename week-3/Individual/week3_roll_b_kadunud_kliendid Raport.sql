97ea4858dfd04313c7444d14962ff904fb4dbb02
germomatt <germo.matt@gmail.com>
2026-07-04 12:26:06 +0300

    Add files via upload

---
+++ week-3/Individual/week3_roll_b_kadunud_kliendid Raport.sql
@@ -0,0 +1,30 @@
+--===========================================================================================================================
+--===================================== KADUNUD KLIENTIDE RAPORT ============================================================
+
+-- TULEMUSED:
+-- Meie kliendibaasi analüüs näitab, et 599 registreerunud klienti pole seni sooritanud 
+-- veel ühtegi ostu. Need passiivsed kasutajad on registreerunud läbivalt kogu vaadeldava 
+-- perioodi jooksul, mis viitab püsivale vajadusele tõhustada uute klientide esmast ostule suunamist. 
+-- Geograafiliselt on probleem kõige kriitilisem Tallinnas (231 klienti) ja Tartus (138 klienti), 
+-- kus asub enam kui pool kõigist kadunud klientidest.
+
+
+-- SOOVITUS TAGASIVÕITMISEKS: 
+-- Nende konverteerimiseks ostjateks soovitan käivitada sihitud e-maili kampaania (keskendudes 
+-- eelkõige Tallinna ja Tartu regioonile), pakkudes neile spetsiaalset piiratud kehtivusajaga 
+-- tervitus- või esmaostusoodustust.
+--===========================================================================================================================
+
+-- KOKKUVÕTE: REGISTREERUMISMUSTRID JA ÄRILISED JÄRELDUSED (URBANSTYLE)
+--=================================================================================================
+--LISAANALÜÜS
+-- 1. TUVASTATUD MUSTRID ANDMETES:
+-- Aastatel 2020–2023 oli uute "kadunud" klientide arv madal ja stabiilne. 
+-- Novembrist 2024 kuni veebruarini 2025 toimus aga plahvatuslik kasv, 
+-- 2024 NOV - 2025 VEEBR tuues nelja kuuga sisse ligi 200 klienti, kes pole teinud ühtegi ostu.
+
+-- 2. ÄRILISED JÄRELDUSED (TURUNDUSKAMPAANIAD):
+-- Talvine hüpe viitab agressiivsetele pühadekampaaniatele, mis ahvatlesid 
+-- lehele "kampaaniaturiste" (nt tasuta hüvede ja looside abil), mitte reaalsete 
+-- ostusoovidega kliente. Ettevõtte klientide hankimine (Lead Gen) on edukas, 
+-- kuid müügilehter pärast registreerumist (konversioon esmaostuks) on ebaefektiivne
